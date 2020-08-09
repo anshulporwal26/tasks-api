29e0c431604ff3852e462f744caafc8323e74005
anshulporwal26 <edysoranshul@gmail.com>
2020-08-09 13:44:02 +0530

    Add user model and controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :name, presence: true, length: { minimum: 5, maximum: 105 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_185025) do
+ActiveRecord::Schema.define(version: 2020_08_09_075621) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
